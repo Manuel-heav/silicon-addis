a67878d42aed05315e38b50d8cfa054e7c4c07b3
manuel-heav <manuelheav32@gmail.com>
2023-07-13 17:40:52 +0300

    working on navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import "./navbar.css"
 const Navbar = () => {
   return (
-    <div className='navbbar'>
-        
+    <div className='navbar'>
+        <div className="container">
+            <div className="links">
+                <h1>SILICON ADDIS</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Studio</li>
+                    <li>Works</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="icons">
+                <button className="button">Hire Now</button>
+            </div>
+        </div>
     </div>
   )
 }
