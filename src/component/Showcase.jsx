68551fb1ec427c6d965853dee6a54c4730a54e78
manuel-heav <manuelheav32@gmail.com>
2023-07-15 18:36:03 +0300

    working on showcase

--- src/component/Showcase.jsx
+++ src/component/Showcase.jsx
@@ -5,14 +5,17 @@ const Showcase = () => {
   return (
     <div className="showcase">
         <Navbar />
-        <div className="showcase__main__conatainer">
+        <div className="showcase__main__container">
           <div className="showcase__main__text">
-            <h1>Programming Courses For Those Who Want to Grow</h1>
+            <h1>Programming Courses For</h1> 
+            <h1 className='highlighted'>Those Who Want to Grow</h1>
             <p>Our courses are designed to accommodate all skill levels. Take your time, pause, rewind, and revisit concepts whenever you need to.</p>
+
+            <button className='showcase__main__btn'>Get Started</button>
           </div>
 
           <div className="showcase__image">
-
+            <img src="/img/moon.png" alt="" />
           </div>
         </div>
         {/* <img src="/img/moon.png" alt="" /> */}
