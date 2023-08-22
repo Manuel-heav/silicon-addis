13c619c474f44b166e946e8b933a88a8c5a61370
manuel-heav <manuelheav32@gmail.com>
2023-08-22 20:07:30 +0300

    working on another new section

--- src/component/Latest.jsx
+++ src/component/Latest.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import "./latest.css"
+import { FaReact } from "react-icons/fa6";
 
 const Latest = () => {
   return (
     <div className='latest'>
-        Latest
+        <div className="latest__container">
+          <div className="latest__container__left">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, dignissimos?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio quod ea delectus molestias eligendi ab quia repudiandae rerum laboriosam aliquam error eveniet, ad voluptates quo saepe quam explicabo earum iste?</p>
+          </div>
+
+          <div className="latest__container__right">
+            <div className="right__cards">
+              <div className="card">
+                <h2>
+                 Lorem ipsum, dolor sit amet consectetur adipisicing.
+                </h2>
+                <FaReact className="card__icon"/>
+              </div>
+
+              <div className="card">
+              <h2>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+                </h2>
+                <FaReact className="card__icon"/>
+              </div>
+
+              <div className="card">
+              <h2>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+                </h2>
+                <FaReact className="card__icon"/>
+              </div>
+
+              <div className="card">
+              <h2>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+                </h2>
+                <FaReact className="card__icon"/>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
