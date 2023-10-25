922c28dfe6af34bebe27588cba5866c96fad5297
manuel-heav <manuelheav32@gmail.com>
2023-10-25 19:09:23 +0300

    working on notify section

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 
-const Navbar = () => {
-  const openMenu = (e) => {
-    e.preventDefault();
-    const nav = document.querySelector('nav');
-    nav.style.display = 'block'
-  }
 
-  const closeMenu = (e) => {
-    e.preventDefault();
-    const nav = document.querySelector('nav');
-    nav.style.top = '-100%';
-  }
+const MobileMenu = () => {
+  return (
+    <div className='mobile-menu'>
+      <a href='#home'>Home</a>
+      <a href='#news'>Youtube</a>
+      <a href='#shop'>Blog</a>
+      <button className="button btn__login">Login</button>
+      <button className="button btn__signup">Sign Up</button>
+    </div>
+  );
+};
+const Navbar = () => {
+  const [isShown, setIsShown] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsShown(!isShown);
+  };
+
   return (
     <div className='navbar'>
-        <div className="container">
+          <div className="container">
             <h1>SILICON ADDIS</h1>
-            <GiHamburgerMenu className="open" onClick={openMenu}/>
             <nav>
-            <AiOutlineClose className="close" onClick={closeMenu}/>
             <div className="links">
                 <ul>
                     <li>Home</li>
@@ -29,27 +33,22 @@ const Navbar = () => {
                     <li>Youtube</li>
                     <li>Blog</li>
                 </ul>
-            </div>
-            <div className="nav__btn__container">
+                <div className="nav__btn__container">
               <button className="button btn__login">Login</button>
               <button className="button btn__signup">Sign Up</button>
             </div>
+            </div>
+            <GiHamburgerMenu className="open" onClick={toggleMobileMenu}/>
             </nav>
-            {/* <div className="mobile_nav">
-            <AiOutlineClose className="close" onClick={closeMenu}/>
-              <div className="mobile_links">
-                  <ul>
-                      <li>Home</li>
-                      <li>Courses</li>
-                      <li>Youtube</li>
-                      <li>Blog</li>
-                <button className="button btn__signup">Sign Up</button>
-                  </ul>
-              </div>
-            </div> */}
+            {isShown && <MobileMenu />}
+              {isShown && (
+                <AiOutlineClose className="close" onClick={toggleMobileMenu}/>
+            )}
         </div>
     </div>
   )
 }
 
 export default Navbar
+
+
--- src/component/Notify.jsx
+++ src/component/Notify.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import "./notify.css"
 
 const Notify = () => {
   return (
     <div className="notify">
         <div className="notify__container">
             <h1>Notify Me Of New Courses</h1>
-            <input type="text" />
+            <div className="notify_input_container">
+                <input type="text" />
+                <button>Notify</button>
+            </div>
         </div>
     </div>
   )
