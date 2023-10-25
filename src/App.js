f7d96cfffc598b483dc58d1cd7a80b4b5fffd895
manuel-heav <manuelheav32@gmail.com>
2023-10-25 10:41:19 +0300

    working on responsiveness

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Showcase from "./component/Showcase";
 import "./App.css";
 import Latest from "./component/Latest";
 import Premium from "./component/Premium";
+import Notify from "./component/Notify";
 function App() {
   return (
     <div className="app">
       <Showcase />
       <Latest />
+      <Notify />
       <Premium />
     </div>
   );
---
+++ src/component/Notify.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Notify = () => {
+  return (
+    <div className="notify">
+        <div className="notify__container">
+            <h1>Notify Me Of New Courses</h1>
+            <input type="text" />
+        </div>
+    </div>
+  )
+}
+
+export default Notify
