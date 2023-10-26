150460eeaefc41d780451e11135cd43a70527d42
manuel-heav <manuelheav32@gmail.com>
2023-10-26 18:35:29 +0300

    working on popular section

--- src/component/Popular.jsx
+++ src/component/Popular.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './popular.css';
+import { AiOutlineArrowRight } from "react-icons/ai";
 const Popular = () => {
 
   return (
     <div className='popular'>
-        <h1>Popular</h1>
-        <div className="youtube__container">
-          
+        <div className="content__container">
+          <div className="text_container">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, iure.</h1>
+            <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore sequi suscipit, blanditiis repellendus ea earum.</h4>
+            <div className="link_btn_container">
+              <a href="#">Check our youtube</a>
+              <AiOutlineArrowRight />
+            </div>
+          </div>
+
+          <div className="img_container">
+            <img src="/img/img4.svg" alt="" />
+          </div>
         </div>
         
     </div>
