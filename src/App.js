7deb0d59ff63e4d9155b1c308f2b0a20fe58dd32
manuel-heav <manuelheav32@gmail.com>
2023-10-26 19:01:52 +0300

    implementing tsparticles

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Latest from "./component/Latest";
 import Premium from "./component/Premium";
 import Notify from "./component/Notify";
 import Popular from "./component/Popular";
+import Design from "./component/Design";
 function App() {
   return (
     <div className="app">
+      <div>
+        <Design />
+      </div>
       <Showcase />
       <Latest />
       <Popular />
---
+++ src/component/Design.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Design = () => {
+  return (
+    <div>Design</div>
+  )
+}
+
+export default Design
